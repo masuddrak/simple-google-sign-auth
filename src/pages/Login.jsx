16877c1e2f95a86a8ac7be3ad538c953a0d9966e
masud <masudranaacb@gmail.com>
2024-03-30 03:19:31 +0600

    added github sign in and google method

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,49 +1,68 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import { app } from "../Firbase/Firebase.init";
 import { useState } from "react";
 
 
 
 const Login = () => {
-    const [signIn,setSignUser]=useState(null)
-    const provider = new GoogleAuthProvider();
+    const [signIn, setSignUser] = useState(null)
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
     const auth = getAuth(app);
-    // sign in 
-    const handelSiginGoogle=()=>{
-        signInWithPopup(auth, provider)
-        .then(result=>{
-            const myUser = result.user;
-            setSignUser(myUser)
-            console.log(myUser)
-        })
-        .catch(error=>{
-            const errorMessage = error.message;
-            console.log(errorMessage)
-        })
+    //google sign in 
+    const handelSiginGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const myUser = result.user;
+                setSignUser(myUser)
+                console.log(myUser)
+            })
+            .catch(error => {
+                const errorMessage = error.message;
+                console.log(errorMessage)
+            })
     }
-    const handelSignOut=()=>{
-        signOut(auth)
+    // github sign in
+    const handelGithubSignIn = () => {
+        signInWithPopup(auth, githubProvider)
         .then(result=>{
-            setSignUser(null)
+            const gitUser=result.user
+            setSignUser(gitUser)
         })
         .catch(error=>{
             console.log(error)
         })
     }
+    const handelSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                setSignUser(null)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
     console.log(signIn)
     return (
         <div className="flext justify-center">
-            {
-                signIn?<button onClick={handelSignOut} className="bg-green-600 p-3 text-center">sign Out google</button> :<button onClick={handelSiginGoogle} className="bg-green-600 p-3 text-center">sig in google</button>
-            }
-          
-           
-           {
-            signIn && <div>
-                <h2 className="text-2xl">{signIn?.displayName}</h2>
-                <img src={signIn?.photoURL} alt="" />
+            <div className="flex gap-8">
+                {
+                    signIn ? <button onClick={handelSignOut} className="bg-green-600 p-3 text-center">sign Out google</button> :
+                        <div className="flex gap-5">
+                            <button onClick={handelSiginGoogle} className="bg-green-600 p-3 text-center">sig in google</button>
+                            <button onClick={handelGithubSignIn} className="bg-green-600 p-3 text-center">sigin Github</button>
+                        </div>
+                }
             </div>
-           }
+
+
+            {
+                signIn && <div>
+                    <h2 className="text-2xl">{signIn?.displayName}</h2>
+                    <h2 className="text-2xl">{signIn?.email}</h2>
+                    <img src={signIn?.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+
+
+const SignUp = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center mt-4">This is Sign-up and Sign in</h1>
+        </div>
+    );
+};
+
+export default SignUp;
